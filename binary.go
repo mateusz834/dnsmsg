189d768af2bfd07b1a1bf11023384ab4828b2512
Mateusz Poliwczak <mpoliwczak34@gmail.com>
2023-07-21 18:09:09 +0200

    add appendUint64

--- binary.go
+++ binary.go
@@ -35,3 +35,16 @@ func appendUint32(b []byte, v uint32) []byte {
 		byte(v),
 	)
 }
+
+func appendUint64(b []byte, v uint64) []byte {
+	return append(b,
+		byte(v>>56),
+		byte(v>>48),
+		byte(v>>40),
+		byte(v>>32),
+		byte(v>>24),
+		byte(v>>16),
+		byte(v>>8),
+		byte(v),
+	)
+}
